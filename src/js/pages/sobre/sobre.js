b59bd4d8a2dac3b198c4b255f3f423cbf635155c
Sarah R. Vieira <sarahrdvieira@gmail.com>
2023-03-31 12:14:27 -0300

    About images added

--- src/js/pages/sobre/sobre.js
+++ src/js/pages/sobre/sobre.js
@@ -5,8 +5,8 @@ export default () => {
   const containerSobre = document.createElement('div');
   const templateSobre = `
   <div class="container-sobre">
-   <p>Desenvolvido por <a class='link' href="https://github.com/daianedosanjos?tab=repositories" target="_blank"> Daiane dos Anjos</a> e  <a href="https://github.com/s2arahrv?tab=repositories" target="_blank"> Sarah Rodrigues</a>, o Olá devas é uma rede social para uso exclusivo de mulheres do setor tecnológico
-  Com o intuíto de diminuir o gap entre homens x mulheres no setor, o olá Devas, foi criado para facilitar a procura de vagas.
+   <p>Desenvolvido por <a class='link' href="https://github.com/daianedosanjos?tab=repositories" target="_blank"> Daiane dos Anjos</a> e  <a href="https://github.com/s2arahrv?tab=repositories" target="_blank"> Sarah Rodrigues</a>, o Olá devas é uma rede social para uso exclusivo de mulheres do setor tecnológico.
+  Com o intuíto de diminuir o gap entre homens x mulheres no setor, o Olá Devas foi criado para facilitar a procura de vagas.
   Nela a usuária poderá criar uma conta de acesso, logar-se com ela, criar, editar, deletar e dar likes em publicações.</p> 
 
   <h1>Desenvolvedoras</h1>
@@ -17,22 +17,21 @@ export default () => {
 
 <ul>
 <li class= "title">Daiane dos Anjos</li>
-<li><a href="https://github.com/daianedosanjos?tab=repositories" target="_blank"> Perfil GitHub
+<li><a href="https://github.com/daianedosanjos?tab=repositories" target="_blank"><img class="link-img" src="./img/github.svg">
            </a> </li>
-<li><a href="https://www.linkedin.com/in/daianeanjos" target="_blank"> Perfil Linkedin
+<li><a href="https://www.linkedin.com/in/daianeanjos" target="_blank"><img class="link-img" src="./img/linkedin.png">
            </a> </li>
 </ul>
 </div>
 <div class="cardItem">
 <a href="https://github.com/daianedosanjos?tab=repositories">
-<img class="card-img" src="./img/daiane.jpg" alt="foto de Sarah">
+<img class="card-img" src="./img/sarah.jpg" alt="foto de Sarah">
 </a> 
 
 <ul>
 <li class= "title">Sarah Rodrigues</li>
-<li><a href="https://github.com/s2arahrv" target="_blank"> Perfil GitHub
-           </a> </li>
-<li><a href="https://github.com/daianedosanjos?tab=repositories" target="_blank"> Perfil Linkedin
+<li><a href="https://github.com/s2arahrv" target="_blank"><img class="link-img" src="./img/github.svg"></a> </li>
+<li><a href="https://www.linkedin.com/in/sarah-rodrigues-251396270/s" target="_blank"><img class="link-img" src="./img/linkedin.png">
            </a> </li>
 </ul>
 </div>
